c137c46a7bfdec439629e8ed4bce6bc2b54817f3
Sudhanshu-06 <sudhanshu767778@gmail.com>
2024-07-30 23:37:30 +0530

    done

---
+++ Mini_project/Password_generator/script.js
@@ -0,0 +1,12 @@
+const inputSlider= document.querySelector('[data-lengthSlider]');
+const lengthDisplay = document.querySelector('[data-lengthNumber]');
+
+const passwordDisplay = document.querySelector('[data-passwordDisplay]');
+const copyBtn = document.querySelector('[data-copy]');
+const copyMsg = document.querySelector('[data-copyMsg]');
+const upperCaseCheck = document.querySelector('#uppercase');
+const lowerCaseCheck = document.querySelector('#lowercase');
+const numberCheck = document.querySelector('#numbers');
+const indicator = document.querySelector('data-indicator');
+const generateBtn = document.querySelector('.generateBtn');
+const allCheckBox = document.querySelector('input[type="checkbox"]');
